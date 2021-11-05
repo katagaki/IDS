8a07d379bceee20f1766d1c08788f90dcb6dc025
katagaki <68231035+katagaki@users.noreply.github.com>
2021-11-05 20:10:48 +0800

    Add clearScreen function

--- SusDetector.java
+++ SusDetector.java
@@ -3,7 +3,12 @@
 class SusDetector {
 
     public static void main(String[] args) {
+        clearScreen();
         System.out.println("Welcome to the SusDetector Intrusion Detection System.");
+    }
+
+    private static void clearScreen() {
+        System.out.print("\033[H\033[2J");
     }
 
 }
