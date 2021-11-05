e978a9e0a2fd3c0fe25220e8ac130ab61f0240e2
katagaki <68231035+katagaki@users.noreply.github.com>
2021-11-05 20:08:11 +0800

    Add placeholder for main class

---
+++ SusDetector.java
@@ -0,0 +1,10 @@
+// The main class for the program
+
+class SusDetector {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the SusDetector Intrusion Detection System.");
+        return 0;
+    }
+
+}
