18d19c095fa0f39c247d86d773ce9508f64b60fe
Mattyssw <matthiasssw@gmail.com>
2021-11-20 23:16:09 +0800

    Update AnalysisEngine.java

---
+++ AnalysisEngine.java
@@ -0,0 +1,21 @@
+public class AnalysisEngine {
+	
+	public int dailyTotal;
+	public Day[] days;
+	public Event[] event;
+	
+	public AnalysisEnginer (Event[] event, Day[] days, int dailyTotal) {
+		
+		this.event = event;
+		this.days = day;
+		this.dailyTotal = dailyTotal;
+		
+	}
+	
+	public double [] AnalysisOfEventStat(String filename) throws FileNotFoundException {
+		
+		
+		
+	}
+	
+}
