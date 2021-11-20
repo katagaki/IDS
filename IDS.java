ddfde1aff86c2e11465bea8a5f12031c79df787c
katagaki <68231035+katagaki@users.noreply.github.com>
2021-11-20 14:16:08 +0800

    Implement stats file parsing

--- IDS.java
+++ IDS.java
@@ -14,11 +14,14 @@ class IDS {
         String numberOfDays = args[2];
         int numberOfEventsInEventsFile = -1;
         int numberOfEventsInStatsFile = -1;
+        Event[] inputEvents = {};
         
         System.out.println("Loading initial event data...");
         try {
             String[] lines = new FileArrayProvider().readLines(Paths.get(eventsFileName).normalize().toString());
-            for (String line : lines) {
+            inputEvents = new Event[lines.length - 1];
+            for (int i = 0; i < lines.length; i++) {
+                String line = lines[i];
                 if (numberOfEventsInEventsFile == -1) {
                     numberOfEventsInEventsFile = Integer.parseInt(line);
                 } else {
@@ -35,14 +38,43 @@ class IDS {
                                                            (eventMin == "" ? true : false),
                                                            (eventMax == "" ? true : false),
                                                            Integer.parseInt(eventWeight));
-                    Event newEvent = new Event(eventName, newEventInfo, new Stats());
+                    inputEvents[i - 1] = new Event(eventName, newEventInfo, new Stats());
                 }
             }
         } catch (IOException e) {
+            System.out.println("!!! Could not parse event data!\n\nStack trace:\n");
             e.printStackTrace();
         }
         
         System.out.println("Loading initial event stats data...");
+        try {
+            String[] lines = new FileArrayProvider().readLines(Paths.get(statsFileName).normalize().toString());
+            for (String line : lines) {
+                if (numberOfEventsInStatsFile == -1) {
+                    numberOfEventsInStatsFile = Integer.parseInt(line);
+                } else {
+                    // Parse line split by colons
+                    String[] lineSplitByColon = line.split(":");
+                    String eventName = lineSplitByColon[0];
+                    String eventMean = lineSplitByColon[1];
+                    String eventStdDev = lineSplitByColon[2];
+                    Stats newStats = new Stats(Double.parseDouble(eventMean),
+                                               Double.parseDouble(eventStdDev));
+                    for (Event event : inputEvents) {
+                        if (event.name == eventName) {
+                            event.stats = newStats;
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("!!! Could not parse event stats data!\n\nStack trace:\n");
+            e.printStackTrace();
+        }
+        
+        System.out.println("Number of events loaded: " + String.valueOf(inputEvents.length));
+        
         // TODO: Check initial input for consistencies
         //       - Ensure that number of events match the number specified in the file
         //       - Ensure that number of events match between 2 files
