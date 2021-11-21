f89d6eb320f9cfa22959bcf34e093bbc15ced1bf
katagaki <68231035+katagaki@users.noreply.github.com>
2021-11-21 21:03:52 +0800

    Update integration in main class

--- IDS.java
+++ IDS.java
@@ -12,40 +12,32 @@ class IDS {
         String eventsFileName = args[0];
         String statsFileName = args[1];
         int numberOfDays = Integer.parseInt(args[2]);
-        int numberOfEventsDeclaredInEventsFile = -1;
-        int numberOfEventsDeclaredInStatsFile = -1;
-        int numberOfEventsParsedInEventsFile = 0;
-        int numberOfEventsParsedInStatsFile = 0;
         Event[] inputEvents = {};
+        String consistencyCheckReport = "";
+        String[] eventsLines = new String[]{};
+        String[] statsLines = new String[]{};
         
         // TODO: Validate command line arguments
         
         System.out.print("Loading initial event data...");
         try {
-            String[] lines = new FileArrayProvider().readLines(Paths.get(eventsFileName).normalize().toString());
-            inputEvents = new Event[lines.length - 1];
-            for (int i = 0; i < lines.length; i++) {
-                String line = lines[i];
-                if (numberOfEventsDeclaredInEventsFile == -1) {
-                    numberOfEventsDeclaredInEventsFile = Integer.parseInt(line);
-                } else {
-                    // Parse line split by colons
-                    String[] lineSplitByColon = line.split(":");
-                    if (lineSplitByColon.length == 5) {
-                        numberOfEventsParsedInEventsFile += 1;
-                        String eventName = lineSplitByColon[0];
-                        String eventType = lineSplitByColon[1];
-                        String eventMin = lineSplitByColon[2];
-                        String eventMax = lineSplitByColon[3];
-                        String eventWeight = lineSplitByColon[4];
-                        EventInfo newEventInfo = new EventInfo((eventType.equals("C") ? EventType.Continuous : EventType.Discrete),
-                                                               (eventMin.equals("") ? 0.0 : Double.parseDouble(eventMin)),
-                                                               (eventMax.equals("") ? 0.0 : Double.parseDouble(eventMax)),
-                                                               (eventMin.equals("") ? false : true),
-                                                               (eventMax.equals("") ? false : true),
-                                                               Integer.parseInt(eventWeight));
-                        inputEvents[i - 1] = new Event(eventName, newEventInfo, new Stats());
-                    }
+            eventsLines = new FileArrayProvider().readLines(Paths.get(eventsFileName).normalize().toString());
+            inputEvents = new Event[eventsLines.length - 1];
+            for (int i = 1; i < eventsLines.length; i++) {
+                String[] lineSplitByColon = eventsLines[i].split(":");
+                if (lineSplitByColon.length == 5) {
+                    String eventName = lineSplitByColon[0];
+                    String eventType = lineSplitByColon[1];
+                    String eventMin = lineSplitByColon[2];
+                    String eventMax = lineSplitByColon[3];
+                    String eventWeight = lineSplitByColon[4];
+                    EventInfo newEventInfo = new EventInfo((eventType.equals("C") ? EventType.Continuous : EventType.Discrete),
+                                                           (eventMin.equals("") ? 0.0 : Double.parseDouble(eventMin)),
+                                                           (eventMax.equals("") ? 0.0 : Double.parseDouble(eventMax)),
+                                                           (eventMin.equals("") ? false : true),
+                                                           (eventMax.equals("") ? false : true),
+                                                           Integer.parseInt(eventWeight));
+                    inputEvents[i - 1] = new Event(eventName, newEventInfo, new Stats());
                 }
             }
         } catch (IOException e) {
@@ -56,25 +48,20 @@ class IDS {
         
         System.out.print("Loading initial event stats data...");
         try {
-            String[] lines = new FileArrayProvider().readLines(Paths.get(statsFileName).normalize().toString());
-            for (String line : lines) {
-                if (numberOfEventsDeclaredInStatsFile == -1) {
-                    numberOfEventsDeclaredInStatsFile = Integer.parseInt(line);
-                } else {
-                    // Parse line split by colons
-                    String[] lineSplitByColon = line.split(":");
-                    if (lineSplitByColon.length == 3) {
-                        numberOfEventsParsedInStatsFile += 1;
-                        String eventName = lineSplitByColon[0];
-                        String eventMean = lineSplitByColon[1];
-                        String eventStdDev = lineSplitByColon[2];
-                        Stats newStats = new Stats(Double.parseDouble(eventMean),
-                                                   Double.parseDouble(eventStdDev));
-                        for (Event event : inputEvents) {
-                            if (event.name.equals(eventName)) {
-                                event.stats = newStats;
-                                break;
-                            }
+            statsLines = new FileArrayProvider().readLines(Paths.get(statsFileName).normalize().toString());
+            for (String line : statsLines) {
+                // Parse line split by colons
+                String[] lineSplitByColon = line.split(":");
+                if (lineSplitByColon.length == 3) {
+                    String eventName = lineSplitByColon[0];
+                    String eventMean = lineSplitByColon[1];
+                    String eventStdDev = lineSplitByColon[2];
+                    Stats newStats = new Stats(Double.parseDouble(eventMean),
+                                               Double.parseDouble(eventStdDev));
+                    for (Event event : inputEvents) {
+                        if (event.name.equals(eventName)) {
+                            event.stats = newStats;
+                            break;
                         }
                     }
                 }
@@ -87,38 +74,22 @@ class IDS {
         
         System.out.println("Number of events loaded: " + String.valueOf(inputEvents.length));
         
+        // Check consistency between events and event stats file
         System.out.println("Checking for inconsistencies...");
-        // Ensure that number of events declared match between 2 files
-        if (numberOfEventsDeclaredInEventsFile != numberOfEventsDeclaredInStatsFile) {
-            System.out.println("!!! Number of events declared in event data (" + String.valueOf(numberOfEventsDeclaredInEventsFile) + ") does not match the number of events declared in the event stats data (" + String.valueOf(numberOfEventsDeclaredInStatsFile) + ")!");
+        consistencyCheckReport = checkConsistency(eventsLines, statsLines);
+        if (consistencyCheckReport.equals("")) {
+            System.out.println("Consistency check complete. No inconsistencies found.");
+        } else {
+            System.out.println("Consistency check failed. Inconsistencies found:\n\n" + consistencyCheckReport + "\n");
         }
-        // Ensure that number of events match between 2 files
-        if (numberOfEventsParsedInEventsFile != numberOfEventsParsedInStatsFile) {
-            System.out.println("!!! Number of events parsed from the event data (" + String.valueOf(numberOfEventsParsedInEventsFile) + ") does not match the number of events parsed from the event stats data (" + String.valueOf(numberOfEventsParsedInStatsFile) + ")!");
-        }
-        // Ensure that number of events match the numbers specified in the files
-        if (numberOfEventsDeclaredInEventsFile != numberOfEventsParsedInEventsFile) {
-            System.out.println("!!! Number of events declared in the event data (" + String.valueOf(numberOfEventsDeclaredInEventsFile) + ") does not match the number of events parsed (" + String.valueOf(inputEvents.length) + ")!");
-        }
-        if (numberOfEventsDeclaredInStatsFile != numberOfEventsParsedInStatsFile) {
-            System.out.println("!!! Number of events declared in the event stats data (" + String.valueOf(numberOfEventsDeclaredInStatsFile) + ") does not match the number of events parsed (" + String.valueOf(inputEvents.length) + ")!");
-        }
-        System.out.println("Consistency check complete.");
-        
-        // TODO: Consistency checks
-        //       - Minimum must be smaller than maximum
-        //       - Mean must be between minimum and maximum
-        //       - Continuous minimum and maximum must be provided *
-        //       - Discrete must be integer
-        //       - Continous events must be 2 decimal places
-        //       - All values must be positive
         
         // Generate 'baseline' data: the initial set of per-day event statistics that are considered acceptable
         System.out.println("Generating 'baseline' data...");
         ActivityEngine activityEngine = new ActivityEngine(inputEvents, numberOfDays);
         activityEngine.generateEvents();
         
-        // TODO: Run analysis engine on 'baseline' data
+        // Run analysis engine on 'baseline' data
+        System.out.println("Generating analyzed data based on 'baseline' data...");
         AnalysisEngine analysisEngine = new AnalysisEngine(activityEngine.generatedDays);
         analysisEngine.analyze();
         analysisEngine.saveStatsFile();
@@ -130,6 +101,62 @@ class IDS {
         while (!isProgramGoingToQuit) {
             isProgramGoingToQuit = true;
         }
+        
+    }
+    
+    public static String checkConsistency(String[] eventsLines, String[] statsLines) {
+        String inconsistencies = "";
+        int numberOfEventsDeclaredInEventsFile = Integer.parseInt(eventsLines[0]);
+        int numberOfEventsDeclaredInStatsFile = Integer.parseInt(statsLines[0]);
+        int numberOfEventsParsedInEventsFile = 0;
+        int numberOfEventsParsedInStatsFile = 0;
+        
+        // Get the number of parsable events in events file
+        for (int i = 1; i < eventsLines.length; i++) {
+            String line = eventsLines[i];
+            String[] lineSplitByColon = line.split(":");
+            if (lineSplitByColon.length == 5) {
+                numberOfEventsParsedInEventsFile += 1;
+            }
+        }
+        
+        // Get the number of parsable events in stats file
+        for (int i = 1; i < statsLines.length; i++) {
+            String line = statsLines[i];
+            String[] lineSplitByColon = line.split(":");
+            if (lineSplitByColon.length == 3) {
+                numberOfEventsParsedInStatsFile += 1;
+            }
+        }
+        
+        // Ensure that number of events declared match between 2 files
+        if (numberOfEventsDeclaredInEventsFile != numberOfEventsDeclaredInStatsFile) {
+            inconsistencies = inconsistencies + ("!!! Number of events declared in event data (" + String.valueOf(numberOfEventsDeclaredInEventsFile) + ") does not match the number of events declared in the event stats data (" + String.valueOf(numberOfEventsDeclaredInStatsFile) + ")!\n");
+        }
+        // Ensure that number of events match between 2 files
+        if (numberOfEventsParsedInEventsFile != numberOfEventsParsedInStatsFile) {
+            inconsistencies = inconsistencies + ("!!! Number of events parsed from the event data (" + String.valueOf(numberOfEventsParsedInEventsFile) + ") does not match the number of events parsed from the event stats data (" + String.valueOf(numberOfEventsParsedInStatsFile) + ")!\n");
+        }
+        // Ensure that number of events match the numbers specified in the files
+        if (numberOfEventsDeclaredInEventsFile != numberOfEventsParsedInEventsFile) {
+            inconsistencies = inconsistencies + ("!!! Number of events declared in the event data (" + String.valueOf(numberOfEventsDeclaredInEventsFile) + ") does not match the number of events parsed (" + String.valueOf(numberOfEventsParsedInEventsFile) + ")!\n");
+        }
+        if (numberOfEventsDeclaredInStatsFile != numberOfEventsParsedInStatsFile) {
+            inconsistencies = inconsistencies + ("!!! Number of events declared in the event stats data (" + String.valueOf(numberOfEventsDeclaredInStatsFile) + ") does not match the number of events parsed (" + String.valueOf(numberOfEventsParsedInStatsFile) + ")!\n");
+        }
+        
+        // TODO: Other consistency checks
+        //       - Minimum must be smaller than maximum
+        //       - Mean must be between minimum and maximum
+        //       - Continuous minimum and maximum must be provided *
+        //       - Discrete must be integer
+        //       - Continous events must be 2 decimal places
+        //       - All values must be positive
+        
+        return inconsistencies;
+    }
+    
+    public static void saveDayFile(Day day, String outputFileName) {
         
     }
     
