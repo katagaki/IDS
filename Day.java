1059a517c1da09f6097cbc27cf85b5cee2f5ea8b
katagaki <68231035+katagaki@users.noreply.github.com>
2021-11-21 20:06:43 +0800

    Add analyzed value to Day class

--- Day.java
+++ Day.java
@@ -11,18 +11,17 @@ public class Day {
         String output = "";
         
         for (Event event : events) {
+            DecimalFormat df = new DecimalFormat("#.00");
             output = output + event.name + ":";
             switch (event.info.eventType) {
                 case Discrete:
-                    output = output + Integer.toString(event.intValue);
+                    output = output + Integer.toString(event.intValue) + ":";
                     break;
                 case Continuous:
-                    DecimalFormat df = new DecimalFormat("#.00");
-                    String doubleFormatted = df.format(event.doubleValue);
-                    output = output + doubleFormatted;
+                    output = output + df.format(event.doubleValue) + ":";
                     break;
             }
-            output = output + "\n";
+            output = output + df.format(event.analyzedValue) + ":\n";
         }
         
         return output;
