70f59c33ffb5b786c7cd781c12f775cf92209c00
zggy <91361679+zggy@users.noreply.github.com>
2021-11-22 23:05:18 +0800

    Update AlertEngine.java

--- AlertEngine.java
+++ AlertEngine.java
@@ -8,7 +8,7 @@ class AlertEngine{
 	public Stats newStats;
 	public Event[] baseEvents[];
     public Day[] generatedDays;
-	public int threshold;
+	public int threshold = 0;
 	public Map<String, Double> dailyCounter = new HashMap<String, Double>();
 	public Map<String, Double> averages = new HashMap<String, Double>();
 	public Map<String, Double> stdDevs = new HashMap<String, Double>();
@@ -21,6 +21,9 @@ class AlertEngine{
 		this.generatedDays = generatedDays;
 		this.averages = averages;
 		this.stdDevs = stdDevs;
+		for (int i = 0; i < baseEvents.length;i++){
+			threshold += baseEvents.events.info.weight * 2;
+		}
 	}
 	
 	public void genDailyCount(){
@@ -28,28 +31,35 @@ class AlertEngine{
 		for (int i  = 0; i < generatedDays.length;i++){
 			Double sum = 0.0;
 			for (int j = 0; j < generatedDays.Event.length;j++){
-				Double eventAvg = 0.0;
-				Double eventStdDev = 0.0;
-				for (Map<String, Double> key : averages.entrySet()){
-					if (key.containsKey(generatedDays.Event.Name)){
-						eventAvg = key.getValue;
-					}
-				}
-				for (Map<String, Double> key : stdDevs.entrySet()){
-					if (key.containsKey(generatedDays.Event.Name)){
-						eventStdDev = key.getValue;
-					}
-				}
+				Double eventAvg = averages.get(generatedDays.events.name);
+				Double eventStdDev = stdDevs.get(generatedDays.events.name);
 				if (event.info.eventType == EventType.Discrete) {
 					sum += (((double)event.intValue - eventAvg)/ eventStdDev) * generatedDays.events.info.weight;
 				} 
 				else {
-					sum += sum += ((event.DoubleValue - eventAvg)/ eventStdDev) * generatedDays.events.info.weight;
+					sum += ((event.DoubleValue - eventAvg)/ eventStdDev) * generatedDays.events.info.weight;
 				}
 				String temp = "Days " + (i+1);
 				dailyCounter.put(temp,sum);
 			}
 		}
+	}
+	
+	public void detectAnomaly(){
+		String temp = "";
+		for (int i = 0; i < dailyCounter.length;i++){
+			temp = "Days " + i;
+			Double counter = dailyCounter.get("temp";)
+			System.out.println(temp + " : " + counter);
+			if (counter < threshold){
+				System.out.println("Ok");
+			}
+			else{
+				System.out.println("Alert");
+			}
+			System.out.println("\n");
+		}
+	}
 	}
 	
 }
