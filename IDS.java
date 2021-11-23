4779f4a2d06f13c95d34850af017ba77b0b2db24
katagaki <68231035+katagaki@users.noreply.github.com>
2021-11-23 22:28:06 +0800

    Update IDS.java

--- IDS.java
+++ IDS.java
@@ -115,6 +115,7 @@ class IDS {
             } else {
                 System.out.println("Consistency check failed. Inconsistencies found:\n\n" + consistencyCheckReport);
             }
+            System.out.println();
             
             // Run activity engine again to generate new set of data for analysis
             ActivityEngine secondaryActivityEngine = new ActivityEngine(inputEvents, newNumberOfDays);
